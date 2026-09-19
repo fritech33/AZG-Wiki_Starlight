2d2da667f64e099d61e3ac4b3800aabb1268095a
fritech33 <80953468+fritech33@users.noreply.github.com>
2026-09-19 13:17:50 +0200

    extend localization

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,11 @@
-import tailwindcss from "@tailwindcss/vite";
-
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
-//import starlightThemeNova from 'starlight-theme-nova'
 import starlightThemeExquisitus from 'starlight-theme-exquisitus';
 
-import { defineCollection } from 'astro:content';
-import { docsLoader, i18nLoader } from '@astrojs/starlight/loaders';
-import { docsSchema, i18nSchema } from '@astrojs/starlight/schema';
+import tailwindcss from "@tailwindcss/vite";
 
-export const collections = {
-  docs: defineCollection({ loader: docsLoader(), schema: docsSchema() }),
-  i18n: defineCollection({ loader: i18nLoader(), schema: i18nSchema() }),
-};
-
-// https://astro.build/config
 export default defineConfig({
     site: 'https://fritech33.github.io',
     base: '/AZG-Wiki_Starlight',
@@ -30,8 +19,13 @@ export default defineConfig({
 					lang: 'de',
 				},
 			},
- 		    //defaultLocale: 'de',
-			//social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			social: [
+				{
+					icon: 'email',
+					label: 'Feedback',
+					href: 'mailto:simon.lauber@gmx.ch'
+				}
+			],
 			sidebar: [
 				{
 					label: 'Übersicht',
@@ -49,7 +43,6 @@ export default defineConfig({
 				'./src/styles/global.css',
 			],
 			plugins: [
-			    //starlightThemeNova(), 
 			    starlightThemeExquisitus(),
 			],
 		}),
